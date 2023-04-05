f580aa37b5cd852a332b7dd66f48a74568ffe1a6
SorioM <128572274+SorioM@users.noreply.github.com>
2023-04-05 01:52:34 -0700

    Add files via upload

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -25,7 +25,11 @@ const fruits =
 
 const wordText = document.querySelector(".word"),
  hintText = document.querySelector(".hint span");
+ inputField = document.querySelector("input");
 skipBtn = document.querySelector(".skip-word");
+enterBtn = document.querySelector(".enter-word");
+
+let correctAnswer;
 
 const game = () => {
     let randomObj = fruits[Math.floor(Math.random() * fruits.length)];
@@ -37,8 +41,18 @@ const game = () => {
 
     wordText.innerText = wordArray.join("");
     hintText.innerText = randomObj.hint;
+    correctAnswer = randomObj.word.toLowerCase();
     console.log(randomObj);
 }
 game();
 
+const inputWord = () => {
+    let userWord = inputField.value.toLocaleLowerCase();
+    if(!userWord) return alert('Please enter your answer.');
+    if(userWord !== correctAnswer) return alert('The Words that you entered is not correct.');
+    alert("Wow! You're answer is correct!");
+    game();
+}
+
 skipBtn.addEventListener("click", game);
+enterBtn.addEventListener("click", inputWord);
