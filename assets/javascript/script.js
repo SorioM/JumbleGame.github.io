c6c1bcfb4f3c01f00e2fc14a9b78b443abf534a5
SorioM <128572274+SorioM@users.noreply.github.com>
2023-04-09 02:59:15 -0700

    Add files via upload

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -49,10 +49,70 @@ game();
 const inputWord = () => {
     let userWord = inputField.value.toLocaleLowerCase();
     if(!userWord) return alert('Please enter your answer.');
-    if(userWord !== correctAnswer) return alert('The Words that you entered is not correct.');
+    if(userWord !== correctAnswer) return alert('Your answer is incorrect.');
     alert("Wow! You're answer is correct!");
     game();
 }
 
 skipBtn.addEventListener("click", game);
 enterBtn.addEventListener("click", inputWord);
+
+//Animals Script
+const animals = 
+[
+    {
+        animalsWord:"monkey",
+        animalsHint: "This mammal are really intelligent and have opposable thumbs, which makes them similar to humans."
+        
+    },
+    {
+        animalsWord:"elephant",
+        animalsHint: "They are the largest land mammals on earth and have distinctly massive bodies, large ears, and long trunks."
+    },
+    {
+        animalsWord:"turtle",
+        animalsHint: "It is a reptile that has a shell covering its body. They are known for moving very slowly."
+    },
+    {
+        animalsWord:"panda",
+        animalsHint: "A black-and-white bear, has a body typical of bears. ."
+    },
+    {
+        animalsWord:"crocodile",
+        animalsHint: "It is a kind of reptile that has tough skin composed of many large plates and scales. ."
+    },
+]
+
+const wordText1 = document.querySelector(".animals-word"),
+ hintText1 = document.querySelector(".animals-hint span");
+ inputField1 = document.querySelector("#gtext1");
+skipBtn1 = document.querySelector(".skip-word1");
+enterBtn1 = document.querySelector(".enter-word1");
+
+let correctAnswer1;
+
+const game1 = () => {
+    let randomObj1 = animals[Math.floor(Math.random() * animals.length)];
+    let wordArray1 = randomObj1.animalsWord.split("");
+    for (let a = wordArray1.length -1 ; a > 0 ; a--){
+        let b = Math.floor(Math.random() * (a * 1));
+        [wordArray1[a], wordArray1[b]] = [wordArray1[b], wordArray1[a]]
+    }
+
+    wordText1.innerText = wordArray1.join("");
+    hintText1.innerText = randomObj1.animalsHint;
+    correctAnswer1 = randomObj1.animalsWord.toLowerCase();
+    console.log(randomObj1);
+}
+game1();
+
+const inputWord1 = () => {
+    let userWord = inputField1.value.toLocaleLowerCase();
+    if(!userWord) return alert('Please enter your answer.');
+    if(userWord !== correctAnswer1) return alert('Your answer is incorrect.');
+    alert("Wow! You're answer is correct!");
+    game1();
+}
+
+skipBtn1.addEventListener("click", game1);
+enterBtn1.addEventListener("click", inputWord1);
